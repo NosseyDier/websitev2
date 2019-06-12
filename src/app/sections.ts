b7300d91c180b87d2b14b071d649a6ca218fd5ee
NosseyDier <reidyesson@gmail.com>
2019-06-12 16:41:37 -0500

    1 file modified

--- src/app/sections.ts
+++ src/app/sections.ts
@@ -10,8 +10,13 @@ export const sections = [
   },
   {
     name: 'Experience',
-    description: 'H'
+    description: 'I am currently an Engineering Intern at Union Pacific. During the semester, I worked at the Robotics Insitute on a computer imaging project for traffic intersections. I previously worked on analyzing the communication between connected vehicles at the USDOT Volpe Center, and worked with the Senseable City Lab to help deploy sensors on garbage trucks in Cambridge.'
   }
+  {
+    name: 'Technical Skills',
+    description: 'I am proficient using Python, C, and Java. I am comfortable with Raspberry Pi, SystemVerilog, computer vision techniques, and HTML/CSS.'
+  }
+
 ];
 
 
